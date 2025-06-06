297198c46ee77d9389d6524fe6a0303031c76852
Matthew Morgenegg <noxpert@gmail.com>
2025-06-06 17:35:24 -0600

    Further reduce duplication

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -4,6 +4,32 @@ import { VocabWord } from "../types/VocabWord";
 
 const MAX_COUNT = 25;
 
+function errorResponse(response: Response, status: number, message: string) {
+  response.send(
+    JSON.stringify(
+      {
+        success: false,
+        status: status,
+        type: "application/json",
+        message: message
+      }
+    )
+  );
+}
+
+function successResponse(response: Response, status: number, message: string|VocabWord[]) {
+  response.send(
+    JSON.stringify(
+      {
+        success: true,
+        status: status,
+        type: "application/json",
+        message: message
+      }
+    )
+  );
+}
+
 export const getWords = (getRequest: Request, getResponse: Response) => {
   if (getRequest.query.count) {
     return getSomeWords(getRequest, getResponse);
@@ -16,25 +42,9 @@ export const getAllWords = (getRequest: Request, getResponse: Response) => {
   let words: VocabWord[] = getVocabWords();
   if (words.length === 0) {
     // No words found
-    getResponse.send(
-      JSON.stringify({
-        success: false,
-        status: 404,
-        type: "application/text",
-        error: "Failed to find any words."
-      })
-    );
+    errorResponse(getResponse, 404, "Failed to find any words.");
   } else {
-    getResponse.send(
-      JSON.stringify(
-        {
-          success: true,
-          status: 200,
-          type: "application/json",
-          message: words,
-        }
-      )
-    );
+    successResponse(getResponse, 200, words);
   }
 }
 
@@ -42,55 +52,24 @@ export const getSomeWords = (getRequest: Request, getResponse: Response) => {
   // let words: VocabWord[] = getVocabWords();
   const count = Number(getRequest.query.count);
 
-  function errorResponse(status: number, message: string) {
-    getResponse.send(
-      JSON.stringify(
-        {
-          success: false,
-          status: status,
-          type: "application/json",
-          message: message
-        }
-      )
-    );
-  }
-
   if (isNaN(count)) {
     console.log(`GET with non-numeric count: ${count}`);
-    errorResponse(400, `Invalid count: ${count}`);
+    errorResponse(getResponse, 400, `Invalid count: ${count}`);
   } else if (typeof getRequest.query.count === 'string' && getRequest.query.count.includes(".")) {
     console.log(`GET with non integer count: ${count}`);
-    errorResponse(400, `Invalid count: ${count}`);
+    errorResponse(getResponse, 400, `Invalid count: ${count}`);
   } else if (count <= 0) {
     console.log(`GET count must be greater than zero, received: ${count}`);
-    errorResponse(400, `Invalid count: ${count}`);
+    errorResponse(getResponse, 400, `Invalid count: ${count}`);
   } else if (count > MAX_COUNT) {
     console.log(`GET count must be less than ${MAX_COUNT}, received: ${count}`);
-    errorResponse(400, `Invalid count: ${count}`);
+    errorResponse(getResponse, 400, `Invalid count: ${count}`);
   } else {
     console.log(`GET with count of: ${getRequest.query.count}`);
-    getResponse.send(
-      JSON.stringify(
-        {
-          success: true,
-          status: 200,
-          type: "application/json",
-          message: "GET some words",
-        }
-      )
-    );
+    successResponse(getResponse, 200, "GET some words");
   }
 }
 
 export const addWord = (getRequest: Request, postResponse: Response) => {
-  postResponse.send(
-    JSON.stringify(
-      {
-        success: false,
-        status: 404,
-        type: "application/json",
-        message: "The POST to add words is not yet implemented"
-      }
-    )
-  );
+  errorResponse(postResponse, 404, "The POST to add words is not yet implemented");
 }
